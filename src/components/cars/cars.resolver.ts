59fdcf4b00f87684f0b23069afea89a4f4df612c
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-12 07:02:47 +0100

    Resolved the car entity to be queried from the API.

--- src/components/cars/cars.resolver.ts
+++ src/components/cars/cars.resolver.ts
@@ -1,5 +1,6 @@
 import { Query, Resolver } from "@nestjs/graphql"
 import { CarsService } from "./cars.service";
+import { Car } from "./entities/car";
 
 @Resolver()
 export class CarsResolver {
@@ -7,8 +8,10 @@ export class CarsResolver {
 
     }
 
-    @Query(returns => String)
-    public async cars() {
-        return "Hello and welcome to Yourcar shop!";
+    @Query(returns => [Car])
+    public async cars(): Promise<Car[]> {
+        return await this.carsService.getAllCars().catch((err) => {
+            throw err;
+        });
     }
 }
