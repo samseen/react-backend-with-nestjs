3abd2bf9374a9a35e6070fd40a983e329dced2eb
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-13 09:21:23 +0100

    Created a new car mutation with GQL input.

--- src/components/cars/cars.service.ts
+++ src/components/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { NewCarInput } from "./dto/new-car.input";
 import { Car } from "./entities/car";
 
 @Injectable()
@@ -15,4 +16,11 @@ export class CarsService {
         });
     }
 
+    public async addCar(newCarData: NewCarInput): Promise<Car> {
+        const newCar = this.carRepository.create(newCarData);
+        await this.carRepository.save(newCar).catch((err) => {
+            new InternalServerErrorException();
+        });
+        return newCar;
+    }
 }
